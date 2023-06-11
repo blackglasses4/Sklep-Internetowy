baa823a43a2e0c59d1a1771f2ee8593fe24ac440
Mikołaj Hoffmann <e.m4il@op.pl>
2023-06-11 15:22:05 +0200

    Dodanie przycisku Strona główna który będzie przenosił na na początkową stronę

--- admin/include/users.crud.php
+++ admin/include/users.crud.php
@@ -13,34 +13,35 @@ session_start();
 </head>
 
 <body>
-    <h1>Witaj Administratorze!</h1>
-    <button class="btn btn-primary"><a class="text-light" href="../users.add.php">Dodaj użytkownika</a></button><br>
-    <br><button class="btn btn-primary"><a class="text-light" href="../products.crud.php">Zarządzaj produktami</a></button>
-    <div class="container">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">Id</th>
-                    <th scope="col">Nazwa użytkownika</th>
-                    <th scope="col">Email</th>
-                    <th scope="col">Rola</th>
-                </tr>
-            </thead>
-            <tbody>
+<h1>Witaj Administratorze!</h1>
+<a class="btn btn-primary" href="../../index.php">Strona główna</a><br><!-- Dodany przycisk "Strona główna" -->
+<br><button class="btn btn-primary"><a class="text-light" href="../users.add.php">Dodaj użytkownika</a></button><br>
+<br><button class="btn btn-primary"><a class="text-light" href="../../produkty/dodawanieiusuwanie.php">Zarządzaj produktami</a></button>
+<div class="container">
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Nazwa użytkownika</th>
+            <th scope="col">Email</th>
+            <th scope="col">Rola</th>
+        </tr>
+        </thead>
+        <tbody>
 
-                <?php
-                require_once('database.php');
+        <?php
+        require_once('database.php');
 
-                $sql = "SELECT * FROM users";
-                $result = mysqli_query($servername, $sql);
+        $sql = "SELECT * FROM users";
+        $result = mysqli_query($servername, $sql);
 
-                while ($row = mysqli_fetch_assoc($result)) {
-                    $idUsers = $row['idUsers'];
-                    $username = $row['username'];
-                    $email = $row['email'];
-                    $rola = $row['id_role'];
+        while ($row = mysqli_fetch_assoc($result)) {
+            $idUsers = $row['idUsers'];
+            $username = $row['username'];
+            $email = $row['email'];
+            $rola = $row['id_role'];
 
-                    echo '<tr>
+            echo '<tr>
                         <th scope="row">' . $idUsers . '</th>
                         <td>' . $username . '</td>
                         <td>' . $email . '</td>
@@ -50,11 +51,11 @@ session_start();
                             <button class="btn btn-primary"><a class="text-light" href="../users.update.php?updateid=' . $idUsers . '">Zaaktualizuj</a></button>
                             <button class="btn btn-danger"><a class="text-light" href="../users.delete.php?deleteid=' . $idUsers . '">Usuń</a></button>
                         </td>';
-                }
-                ?>
-            </tbody>
-        </table>
-    </div>
+        }
+        ?>
+        </tbody>
+    </table>
+</div>
 </body>
 
 </html>
